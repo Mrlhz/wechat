87fb570e6d7f4757d0ed35d54dbc0436c96f737e
lhz <mr__lhz@163.com>
2018-09-18 01:32:02 +0800

    mock模拟后台数据

---
+++ data/app.js
@@ -0,0 +1,139 @@
+var data = {
+  "status": 200,
+  "msg": "ok",
+  "lst": [
+    {
+      "date": "2018-09-17",
+      "detail": [
+        {
+          "alertsCount": 0,
+          "alertsId": 0,
+          "alertsName": "微信",
+          "alertsType": "app",
+          "blockCount": 0,
+          "count": "10,1,10,2,3,4,6,1",
+          "date": "2018-09-17",
+          "domain": "app:微信",
+          "groupId": "153198462732",
+          "gwMac": "02:01:0F:DA:2A:98",
+          "lastTime": "2018-09-17 21:00:12.0",
+          "onlineTime": 0,
+          "playMinute": 0,
+          "policyAct": 0,
+          "policyStatus": 0,
+          "statTime": "20,14,15,16,09,17,10,21",
+          "time": "21:00:12"
+        },
+        {
+          "alertsCount": 0,
+          "alertsId": 0,
+          "alertsName": "拼多多",
+          "alertsType": "app",
+          "blockCount": 0,
+          "count": "1",
+          "date": "2018-09-17",
+          "domain": "app:拼多多",
+          "groupId": "153468168315",
+          "gwMac": "02:01:0F:DA:2A:98",
+          "lastTime": "2018-09-17 17:13:31.0",
+          "onlineTime": 0,
+          "playMinute": 227,
+          "policyAct": 0,
+          "policyStatus": 0,
+          "statTime": "17",
+          "time": "17:13:31"
+        },
+        {
+          "alertsCount": 0,
+          "alertsId": 0,
+          "alertsName": "淘宝/天猫/闲鱼/盒马鲜生",
+          "alertsType": "app",
+          "blockCount": 0,
+          "count": "4",
+          "date": "2018-09-17",
+          "domain": "app:淘宝/天猫/闲鱼/盒马鲜生",
+          "groupId": "153259095046",
+          "gwMac": "02:01:0F:DA:2A:98",
+          "lastTime": "2018-09-17 15:49:22.0",
+          "onlineTime": 0,
+          "playMinute": 311,
+          "policyAct": 0,
+          "policyStatus": 0,
+          "statTime": "15",
+          "time": "15:49:22"
+        }
+      ]
+    },
+    {
+      "date": "2018-09-14",
+      "detail": [
+        {
+          "alertsCount": 0,
+          "alertsId": 0,
+          "alertsName": "微信",
+          "alertsType": "app",
+          "blockCount": 0,
+          "count": "6,2,8,5,1,1,5,5,1,5,4",
+          "date": "2018-09-14",
+          "domain": "app:微信",
+          "groupId": "153198462732",
+          "gwMac": "02:01:0F:DA:2A:98",
+          "lastTime": "2018-09-14 20:07:52.0",
+          "onlineTime": 0,
+          "playMinute": 3697,
+          "policyAct": 0,
+          "policyStatus": 0,
+          "statTime": "18,17,19,12,20,14,15,16,14,17,16",
+          "time": "20:07:52"
+        }
+      ]
+    },
+    {
+      "date": "2018-09-12",
+      "detail": [
+        {
+          "alertsCount": 0,
+          "alertsId": 0,
+          "alertsName": "微信",
+          "alertsType": "app",
+          "blockCount": 0,
+          "count": "6,2,8,5,1,1,5,5,1,5,4",
+          "date": "2018-09-12",
+          "domain": "app:微信",
+          "groupId": "153198462732",
+          "gwMac": "02:01:0F:DA:2A:98",
+          "lastTime": "2018-09-12 17:42:53.0",
+          "onlineTime": 0,
+          "playMinute": 6722,
+          "policyAct": 0,
+          "policyStatus": 0,
+          "statTime": "18,17,19,12,20,14,15,16,14,17,16",
+          "time": "17:42:53"
+        },
+        {
+          "alertsCount": 0,
+          "alertsId": 0,
+          "alertsName": "腾讯视频",
+          "alertsType": "app",
+          "blockCount": 0,
+          "count": "1,1",
+          "date": "2018-09-12",
+          "domain": "app:腾讯视频",
+          "groupId": "153198049457",
+          "gwMac": "02:01:0F:DA:2A:98",
+          "lastTime": "2018-09-12 14:52:14.0",
+          "onlineTime": 0,
+          "playMinute": 6893,
+          "policyAct": 0,
+          "policyStatus": 0,
+          "statTime": "12,14",
+          "time": "14:52:14"
+        }
+      ]
+    }
+  ]
+}
+
+module.exports = {
+  data: data
+}
