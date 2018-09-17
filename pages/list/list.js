8ad33b8bab76b3ef1c11d029308c08a03e7f31f6
lhz <mr__lhz@163.com>
2018-09-18 01:35:48 +0800

    测试列表

---
+++ pages/list/list.js
@@ -0,0 +1,82 @@
+// pages/list/list.js
+var data = require('../../data/app.js')
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+    console.log(data.data)
+    var { data: { lst: lst } } = data
+    console.log(lst)
+    var result = []
+    lst.map(function(item) {
+      for (let detail of item.detail) {
+        console.log(detail.groupId)
+        result.push(detail.groupId)
+        console.log(result)        
+      }
+    })
+    var result = [...new Set(result)]
+    this.setData({
+      lists: result
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
+
+  }
+})
