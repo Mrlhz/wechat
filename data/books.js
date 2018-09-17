7ed793bab6fe22a59e2ee8ca468df3d7c8c36139
lhz <mr__lhz@163.com>
2018-09-18 01:34:08 +0800

    mock模拟后台数据

--- data/books.js
+++ data/books.js
@@ -1,5 +1,28 @@
 var data = [{
     "author": [
+      "[希] Lea Verou"
+    ],
+    "binding": "平装",
+    "category": "编程",
+    "id": 325,
+    "image": "https://img1.doubanio.com/lpic/s28659699.jpg",
+    "images": {
+      "large": "https://img1.doubanio.com/lpic/s28659699.jpg"
+    },
+    "isbn": "9787115416940",
+    "pages": "260",
+    "price": "99.00元",
+    "pubdate": "2016-4",
+    "publisher": "人民邮电出版社",
+    "subtitle": "",
+    "summary": "本书是一本注重实践的教程，作者为我们揭示了 47 个鲜为人知的 CSS 技巧，主要内容包括背景与边框、形状、 视觉效果、字体排印、用户体验、结构与布局、过渡与动画等。本书将带领读者循序渐进地探寻更优雅的解决方案，攻克每天都会遇到的各种网页样式难题。\\n本书的读者对象为前端工程师、网页开发人员。",
+    "title": "CSS揭秘",
+    "translator": [
+      "CSS魔法"
+    ]
+  },
+  {
+    "author": [
       "[美]尼古拉斯·泽卡斯"
     ],
     "binding": "平装",
