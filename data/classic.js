a44dd87a592268d0ede2069b0411ba9535d15c67
lhz <mr__lhz@163.com>
2018-10-20 23:28:12 +0800

    add mock data

---
+++ data/classic.js
@@ -0,0 +1,97 @@
+// http://bl.7yue.pro/v1/classic/latest?appkey=AbhC31IG7ruCDp57
+// http://bl.7yue.pro/v1/classic/200/4?appkey=O9p281UuYsXImisw
+// http://bl.7yue.pro/v1/classic/7/previous?appkey=O9p281UuYsXImisw
+var classic = {
+  'text': '期刊',
+  data: [{
+      "content": "人生不能像做菜，把所有的料准备好才下锅",
+      "fav_nums": 0,
+      "id": 1,
+    "image": "http://bl.7yue.pro/images/movie.8.png",
+      "index": 7,
+      "like_status": 0,
+      "pubdate": "2018-06-22",
+      "title": "李安<<饮食男女>>",
+      "type": 100
+    }, {
+      "content": "岁月长，衣裳薄",
+      "fav_nums": 59,
+      "id": 3,
+      "image": "http://bl.7yue.pro/images/music.3.png",
+      "index": 1,
+      "like_status": 0,
+      "pubdate": "2018-06-22",
+      "title": "杨千嬅《再见二丁目》",
+      "type": 200,
+      "url": "http://music.163.com/song/media/outer/url?id=317245.mp3"
+    }, {
+      "content": "这个夏天又是一个毕业季",
+      "fav_nums": 53,
+      "id": 2,
+      "image": "http://bl.7yue.pro/images/sentence.2.png",
+      "index": 2,
+      "like_status": 0,
+      "pubdate": "2018-06-22",
+      "title": "未名",
+      "type": 300
+    }, {
+      "content": "你陪我步入蝉夏 越过城市喧嚣",
+      "fav_nums": 73,
+      "id": 2,
+      "image": "http://bl.7yue.pro/images/music.1.png",
+      "index": 3,
+      "like_status": 0,
+      "pubdate": "2018-06-22",
+      "title": "花粥 《纸短情长》",
+      "type": 200,
+      "url": "http://music.163.com/song/media/outer/url?id=557581284.mp3"
+    },
+    {
+      "content": "在变换的生命里，岁月原来是最大的小偷",
+      "fav_nums": 68,
+      "id": 2,
+      "image": "http://bl.7yue.pro/images/movie.4.png",
+      "index": 4,
+      "like_status": 0,
+      "pubdate": "2018-06-22",
+      "title": "罗启锐《岁月神偷》",
+      "type": 100
+    }, {
+      "content": "许多人来来去去，相聚又别离",
+      "fav_nums": 92,
+      "id": 4,
+      "image": "http://bl.7yue.pro/images/music.5.png",
+      "index": 5,
+      "like_status": 0,
+      "pubdate": "2018-06-22",
+      "title": "好妹妹 《一个人的北京》",
+      "type": 200,
+      "url": "http://music.163.com/song/media/outer/url?id=26427662.mp3"
+    }, {
+      "content": "心上无垢，林间有风",
+      "fav_nums": 104,
+      "id": 1,
+      "image": "http://bl.7yue.pro/images/sentence.6.png",
+      "index": 6,
+      "like_status": 0,
+      "pubdate": "2018-06-22",
+      "title": "未名",
+      "type": 300
+    }, {
+      "content": "谁念过 千字文章 秋收冬已藏",
+      "fav_nums": 140,
+      "id": 1,
+      "image": "http://bl.7yue.pro/images/music.7.png",
+      "index": 7,
+      "like_status": 0,
+      "pubdate": "2018-06-22",
+      "title": "不才 《参商》",
+      "type": 200,
+      "url": "http://music.163.com/song/media/outer/url?id=29719651.mp3"
+    }
+  ]
+}
+
+module.exports = {
+  classic: classic
+}
